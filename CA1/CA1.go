158a2dc4b81fd7f0ac1f13421323085aaf261541
Ali Akhgari <61042035+AliAkhgari@users.noreply.github.com>
2023-03-15 20:18:47 +0330

    Add files via upload

---
+++ CA1/CA1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"CA1/phase1"
+	"CA1/phase2"
+	"os"
+)
+
+func main() {
+	phase1.Run("input.txt", "output.txt")
+
+	args := os.Args
+	phase2.Run(args[1], "input.txt", "output.txt")
+}
---
+++ CA1/phase1/1.go
@@ -0,0 +1,69 @@
+package phase1
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func CapitalizeInput(input string) string {
+	re := regexp.MustCompile(`(^|\.|\?|!)\s*(\w)`)
+	capitalizedInput := re.ReplaceAllStringFunc(input, func(s string) string {
+		return strings.ToUpper(s)
+	})
+	return capitalizedInput
+}
+
+func OrdinalizeNumbers(input string) string {
+	re := regexp.MustCompile(`\d+(st|nd|rd|th)?`)
+	ordinalizedNumbers := re.ReplaceAllStringFunc(input, func(s string) string {
+		return ConvertToOrdinal(s)
+	})
+	return ordinalizedNumbers
+}
+
+func ConvertToOrdinal(s string) string {
+	if strings.HasSuffix(s, "st") || strings.HasSuffix(s, "nd") || strings.HasSuffix(s, "rd") || strings.HasSuffix(s, "th") {
+		return s
+	}
+	num, _ := strconv.Atoi(s)
+	lastDigit := num % 10
+	ordinalSuffix := "th"
+	switch lastDigit {
+	case 1:
+		if num%100 != 11 {
+			ordinalSuffix = "st"
+		}
+	case 2:
+		if num%100 != 12 {
+			ordinalSuffix = "nd"
+		}
+	case 3:
+		if num%100 != 13 {
+			ordinalSuffix = "rd"
+		}
+	}
+	return s + ordinalSuffix
+}
+
+func Run(inputFilePath string, outputFilePath string) {
+	inputFile, _ := os.Open(inputFilePath)
+	defer inputFile.Close()
+
+	outputFile, _ := os.Create(outputFilePath)
+	defer outputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
+	writer := bufio.NewWriter(outputFile)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		capitalizedText := CapitalizeInput(text)
+		result := OrdinalizeNumbers(capitalizedText)
+		writer.WriteString(result + "\n")
+	}
+
+	writer.Flush()
+}
